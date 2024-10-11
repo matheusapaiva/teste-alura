a7e73e6ad69f1f13a738095f938d8850f9e48b37
Matheus Paiva <147844006+matheusapaiva@users.noreply.github.com>
2024-10-11 14:04:53 -0300

    Add files via upload

---
+++ desafio_troca.js
@@ -0,0 +1,20 @@
+let a = 7;
+let b = 94;
+
+// let temp = a;
+// a = b;
+// b = temp
+
+// console.log(a);
+// console.log(b)
+
+// Outra forma de realizar a troca de valores é utilizando "[]", assim é possivel que o valor de A seja atribuido ao valor de B e o valor de B, seja atribuido ao valor de A
+
+[a,b] = [b,a]
+
+console.log(a)
+console.log(b);
+
+
+
+
